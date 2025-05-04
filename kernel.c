feed50834edd939dae9f0cd1401dd3b0d68e4a12
green-21 <133c96d@naver.com>
2025-05-04 22:52:07 +0900

    07. 커널 패닉닉

--- kernel.c
+++ kernel.c
@@ -27,9 +27,11 @@ void putchar(char ch) {
 }
 
 void kernel_main(void) {
-    printf("\n\nHello %s\n", "World!");
-    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
-    
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+    PANIC("booted!");
+    printf("unreachable here!\n");
+
     for (;;) {
         __asm__ __volatile__("wfi");
     }
